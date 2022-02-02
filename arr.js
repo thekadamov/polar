0cd892ca02eb8e1990cc23ffa7c7f1c30895f41f
thekadamov <abdulmalikxaitov223@gmail.com>
2022-02-02 19:26:13 +0300

    polar commit

---
+++ arr.js
@@ -0,0 +1,15 @@
+// const button = document.querySelector('nav-btn');
+// button.onclick = function(event){
+//     let x = event.clientX - event.target.offsetLeft;
+//     let y = event.clientY - event.target.offsetTop;
+
+//     let ripple = document.createElement('span');
+//     ripple.style.left = x + 'px';
+//     ripple.style.top = y + 'px';
+//     this.appendChild(ripple);
+
+
+//     setTimeout(() =>{
+//         ripple.remove()
+//     }, 1000);
+// }
